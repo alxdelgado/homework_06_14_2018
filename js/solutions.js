ce19d413bc86920e68a1a82a869d3ff7d7bf0e62
Alex Delgado <delgadoalex@icloud.com>
2018-06-14 18:32:32 -0500

    Secondary Commit

---
+++ js/solutions.js
@@ -0,0 +1,19 @@
+console.log('JS is working'); 
+
+
+///////////////// HOMEWORK ////////////////////
+
+// 1. // 
+// Function parameters are the names listed in the function's definition. // 
+
+// 2. // 
+// Function arguments are the real values passed to the function. // 
+
+// 3. // 
+// Console.log will display the parameter in the console. // 
+// Return statement will stop executing the function and return the specified value. // 
+
+////////////// COMMIT 001 ///////////
+
+
+
